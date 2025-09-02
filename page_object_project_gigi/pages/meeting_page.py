ae879d7e285f39d948b0c33ba662ab2c51fdecf2
Gkvant <gigikvantwork@gmail.com>
2025-09-02 23:49:31 +0300

    Added new page tests

--- page_object_project_gigi/pages/meeting_page.py
+++ page_object_project_gigi/pages/meeting_page.py
@@ -1,3 +1,6 @@
+import re
+
+
 class jwMeetingPage:
     def __init__(self, page):
         self.page = page
@@ -9,5 +12,51 @@ class jwMeetingPage:
     def search_for_(self, input_value):
         search_field = self.page.locator("#searchLocation")
         search_field.fill(input_value)
-        first_search_result = self.page.locator("[class='autocomplete__item__link ng-isolate-scope']").first
-        first_search_result.click()
+
+    def is_result(self, item_text):
+        # Wait for the page to finish loading
+        self.page.wait_for_load_state("networkidle")
+
+        # Possible search result variations
+        possible_results = [
+            item_text,
+            self.alternate_search_result_keep_just_text(item_text),
+            self.alternate_search_result_capitalaize(item_text)
+        ]
+        # Check each variant for matching elements on the page
+        for result in possible_results:
+            matching_elements = self.page.query_selector_all(f"text={result}")
+            if matching_elements:  # Equals to len(result_objects_on_site) > 0
+                return True
+        # No matches found
+        return False
+
+    @staticmethod
+    def alternate_search_result_keep_just_text(input_value):
+        # Replace all non-alphanumeric characters with a space
+        cleaned = re.sub(r'[^a-zA-Z]', ' ', input_value)
+
+        # Collapse multiple spaces and trim
+        return ' '.join(cleaned.split())
+
+    @staticmethod
+    def alternate_search_result_capitalaize(input_value):
+        # Split the input string into individual words - ['tel', 'aviv']
+        words = input_value.split()
+
+        # Capitalize the first letter of each word - ['Tel', 'Aviv']
+        capitalized_words = [word.capitalize() for word in words]
+
+        # Join the capitalized words back into a single string - "Tel Aviv"
+        result = ' '.join(capitalized_words)
+        return result
+
+    def if_correct_tab_shown_after_clicking_(self, current_tab_name):
+        visible_name = current_tab_name.split(' ')[1]
+        self.click_tab_(visible_name)
+        tab_title = self.page.get_by_text(visible_name)
+        return tab_title.count() == 3  # On Page if correct Tab is selected it will find 3 elements, otherwise only 1 element is found
+
+    def click_tab_(self, visible_name):
+        tab = self.page.get_by_text(visible_name).first
+        tab.click()
--- page_object_project_gigi/tests/test_meeting_page.py
+++ page_object_project_gigi/tests/test_meeting_page.py
@@ -1,13 +1,42 @@
 import time
 
+import pytest
+
 from page_object_project_gigi.pages.meeting_page import jwMeetingPage
 from page_object_project_gigi.tests.conftest import setup_jw_org
 
 
-
 class TestMeetingPage:
-
-    def test_search_meeting(self, setup_jw_org):
+    @pytest.mark.parametrize("search_term, expected_result", [
+        ("tel-aviv", True),
+        ("Tel-aviv", True),
+        ("Tel-Aviv", True),
+        ("tel aviv", True),
+        ("Haifa", True),
+        ("Holon", True),
+        ("asdfgh", False),
+        ("Fictional City", False)
+    ])
+    def test_search_result(self, setup_jw_org, search_term, expected_result):
         meeting_page = jwMeetingPage(setup_jw_org)
-        meeting_page.search_for_("Tel-Aviv")
-        # time.sleep(5)
+        meeting_page.search_for_(search_term)
+        search_result = meeting_page.is_result(search_term)
+        assert search_result == expected_result, f"Search term {search_term} caused wrong behavior"
+
+    def test_weekly_meetings_tab(self, setup_jw_org):
+        meetings_tab = "Weekly Meetings"
+        meeting_page = jwMeetingPage(setup_jw_org)
+        is_correct_tab_shown = meeting_page.if_correct_tab_shown_after_clicking_(meetings_tab)
+        assert is_correct_tab_shown, f"Meetings Tab not showing up after click"
+
+    def test_regional_conventions_tab(self, setup_jw_org):
+        conventions_tab = "Regional Conventions"
+        meeting_page = jwMeetingPage(setup_jw_org)
+        is_correct_tab_shown = meeting_page.if_correct_tab_shown_after_clicking_(conventions_tab)
+        assert is_correct_tab_shown, f"Conventions Tab not showing up after click"
+
+    def test_circuit_assemblies_tab(self, setup_jw_org):
+        assembly_tab = "Circuit Assemblies"
+        meeting_page = jwMeetingPage(setup_jw_org)
+        is_correct_tab_shown = meeting_page.if_correct_tab_shown_after_clicking_(assembly_tab)
+        assert is_correct_tab_shown, f"Assemblies Tab not showing up after click"
